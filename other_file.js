0f251887cb8063fecc7606394c6ceee7a6048052
Charles Shorter Jr <c4short27@gmail.com>
2019-04-24 14:23:30 +0000

    Automatically backed up by Learn

--- other_file.js
+++ other_file.js
@@ -1,1 +1,14 @@
-console.log(name)
+javascript
+var index = require("./index.js")
+
+if (name === "Susan") {
+    console.log("The name is correct")
+} else {
+    console.log("Expected the name to be Susan but it is" +index.name)
+}
+
+if (height === "74") {
+    console.log("The height is correct")
+} else {
+    console.log("Expected the height to be 74 but it is" +index.height)
+}
